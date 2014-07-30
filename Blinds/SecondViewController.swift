2c872a51000b4c639ccbc320ebfd162b55c34a93
Josh Meek <joshmeek18@gmail.com>
2014-07-30 00:01:23 -0400

    Began round list

--- Blinds/SecondViewController.swift
+++ Blinds/SecondViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-                            
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var bigBlind: UITextField!
+    @IBOutlet var smallBlind: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,38 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func addRoundTapped() {
+        println("TAPPED!")
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        var cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "")
+        cell.textLabel.text = "Yes"
+        cell.detailTextLabel.text = "No"
+        cell.editing = true
+        cell.setEditing(true, animated: true)
+        //cell
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            
+            tableView.delete(indexPath)
+            
+            tableView.reloadData()
+            // handle delete (by removing the data from your array and updating the tableview)
+        }
+    }
+    
 }
 
